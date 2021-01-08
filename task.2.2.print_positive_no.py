56bae5e55327313564989f19f72607a1ed794e6a
KathaVachhani <vachhani.katha21@gmail.com>
2021-01-08 15:18:10 +0530

    task 2

---
+++ task.2.2.print_positive_no.py
@@ -0,0 +1,14 @@
+lst = []
+n = int(input("Enter number of elements : "))
+
+for i in range(0, n):
+    ele = int(input())
+
+    lst.append(ele)
+
+print(lst)
+
+for num in lst:
+
+    if num >= 0:
+        print(num, end=" ")
